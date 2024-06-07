32de608e57d4de17ac8427a4fecd2a6c41ed3540
tanePON <kazuaki.tanemura@bnt.benextgroup.jp>
2024-06-07 18:19:51 +0900

    a-frameに影響されないボタンをJSで追加

--- dist/scenes/c_anchor/button.js
+++ dist/scenes/c_anchor/button.js
@@ -1,8 +1,7 @@
-/* global AFRAME */
 AFRAME.registerComponent('button', {
   init: function () {
     var buttonContainerEl = this.buttonContainerEl = document.createElement('div');
-    var buttonEl = this.buttonEl = document.createElement('button');
+    var buttonEl = document.createElement('button');
     var style = document.createElement('style');
     var css =
       '.a-button-container {box-sizing: border-box; display: inline-block; height: 34px; padding: 0;;' +
@@ -12,11 +11,7 @@ AFRAME.registerComponent('button', {
       '.a-button {cursor: pointer; padding: 0px 10px 0 10px; font-weight: bold; color: #666; border: 3px solid #666; box-sizing: border-box; vertical-align: middle; max-width: 200px; border-radius: 10px; height: 34px; background-color: white; margin: 0;}' +
       '.a-button:hover {border-color: #ef2d5e; color: #ef2d5e}';
 
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
+    style.appendChild(document.createTextNode(css));
     document.getElementsByTagName('head')[0].appendChild(style);
 
     buttonContainerEl.classList.add('a-button-container');
@@ -24,12 +19,10 @@ AFRAME.registerComponent('button', {
     buttonEl.addEventListener('click', this.onClick.bind(this));
 
     buttonContainerEl.appendChild(buttonEl);
-
     this.el.sceneEl.appendChild(buttonContainerEl);
-    buttonEl.innerHTML = 'TEST BUTTON';
+    buttonEl.innerHTML = 'hoge BUTTON';
   },
-
   onClick: function () {
-
+    console.log("クリックイベントここ");
   }
 });
